04c3922fbd3597605535661fb7f06f51ac1582b5
Christian Diaz <>
2020-02-26 13:13:43 -0500

    Clase Padre figura

---
+++ src/main/java/edu/unicundi/tallerfiguras/Figura.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unicundi.tallerfiguras;
+
+/**
+ *
+ * @author Christian Diaz
+ * @author Cristian Medina
+ */
+public class Figura {
+    
+    private short lado1;
+    private short lado2;
+    private short lado3;
+    
+    
+
+    public short getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(short lado1) {
+        this.lado1 = lado1;
+    }
+
+    public short getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(short lado2) {
+        this.lado2 = lado2;
+    }
+
+    public short getLado3() {
+        return lado3;
+    }
+
+    public void setLado3(short lado3) {
+        this.lado3 = lado3;
+    }
+    
+}
